5ee59791316a4570259d62cf7b3ef8b606314f89
Anastasiya Makarova <asyalapa@mail.ru>
2023-01-17 09:42:55 +0300

    feat: added solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let paragraph = expr.split('**********');
+    let text = ''
+  
+    paragraph.map( itemPar => {
+      
+      let word = []
+      for (let i = 0, j = itemPar.length; i < j; i += 10) { word.push(itemPar.slice(i, i + 10)) }
+      
+      word.map( itemWord => {
+        let letter = []
+        let res = []
+        for (let i = 0, j = itemWord.length; i < j; i += 2) { letter.push(itemWord.slice(i, i + 2)) }
+        
+        letter.map( itemLetter => { (itemLetter === '00') ? ( res.push(' ') ) : ( (itemLetter === '10') ? res.push('.') : res.push('-')) })
+        res = res.join('').split(' ').filter( i => i !== '')
+        
+        res.map( item => { 
+          if (item.length > 5) {
+            for (let i = 0, j = item.length; i < j; i += 5) { 
+              text += MORSE_TABLE[(item.slice(i, i + 5))] }
+          }
+        text += ((MORSE_TABLE[item] != undefined) ? (MORSE_TABLE[item]) : (' '))
+        })
+      })
+      text += ' '
+      })
+      return (text.trim())
 }
 
 module.exports = {
